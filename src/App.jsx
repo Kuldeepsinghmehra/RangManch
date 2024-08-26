b33ab8fe4b9a258d1fb90a91b58423b8a66b1959
kuldeep singh mehra <kuldeepm272@gmail.com>
2024-08-26 17:15:22 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import './App.css'
 import Header from './Header'
 import Meme from './Meme'
+import Feature from './Feature'
 
 function App() {
  
 
   return (
     <div>
+      <Feature />
     <Header/>
     <Meme/>
     </div>
---
+++ src/Feature.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Feature() {
+  return (
+    <div>This is my main feature</div>
+  )
+}
+
+export default Feature
